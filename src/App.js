7025ac78152d5c8c60aca1c20ba100ae99627be2
Danutzz8 <Marius.Prutean@outlook.com>
2021-01-22 01:11:01 +0200

    update our shop

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './components/shop/shop.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-  <h1>HATS PAGE</h1>  
-  </div>
-  );
 
 class App extends React.Component {
   render() {
@@ -18,7 +13,7 @@ class App extends React.Component {
       <div className='App'>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/shop/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     );
